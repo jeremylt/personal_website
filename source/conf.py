f4e3f77b391ebd908751114c0e236563516f30cb
Jeremy L Thompson <jeremy@jeremylt.org>
2021-08-06 16:06:25 -0600

    add icons

--- source/conf.py
+++ source/conf.py
@@ -14,21 +14,17 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
-
 project = ''
 copyright = '2021, Jeremy L Thompson'
 author = 'Jeremy L Thompson'
-
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
@@ -37,7 +33,6 @@ templates_path = ['templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -54,3 +49,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['static']
+html_js_files = [
+    'https://kit.fontawesome.com/4cef94740e.js',
+]
